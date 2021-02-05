9b4222c37ebb1236b6622d7d6cf1e98967333d5b
zhuyuyu <2594851761@qq.com>
2021-02-05 19:39:24 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,5 +3,21 @@
 // 在这个函数中，可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+        // 统一为有权限的接口 设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载complete回调函数
+    options.complete = function(res) {
+        // 在回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1清空token
+            localStorage.removeItem('token')
+                // 2强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 
 })
